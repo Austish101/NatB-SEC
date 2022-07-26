d01024462fcebfc0fd3531cfa7256aa057394930
Austin H <33872825+Austish101@users.noreply.github.com>
2022-07-26 18:23:02 +0100

    Upload basic template files

---
+++ input_data.py
@@ -0,0 +1,8 @@
+# handles the reading and formatting of data for the DNN
+
+def read_file(filename):
+    data = "todo_read"
+    return data
+
+
+
---
+++ training.py
@@ -0,0 +1,3 @@
+# handle the training of the DNN
+
+# file data must be fed into the DNN at a variable speed, it will be slower than real time to start with
