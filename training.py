977a06be59c16ecb4ff7d81a7d0857e21230f38c
pi <auastin.dharper@outlook.com>
2022-08-15 14:27:12 +0100

    - fix a few things with l_dev merge, primarily the output shape (3 binary errors, instead of 1 int) - flattened input data - standardised data

--- training.py
+++ training.py
@@ -4,6 +4,7 @@
 import input_data
 import RNN
 import tensorflow as tf
+import numpy as np
 
 
 # get the expected output given input packets: the next error type and timestamp
@@ -22,7 +23,8 @@ def outputs_given_inputs(input_data, output_data):
             # error type
             chrtemp = ""
             strtemp = ""
-            for c in pkt.DATA.data_data.split(':'):
+            # for c in pkt.DATA.data_data.split(':'):
+            for c in output_data[n][1].data_data.split(':'):
                 try:
                     chrtemp = chr(int(c, 16))
                     strtemp = strtemp + chrtemp
@@ -31,7 +33,7 @@ def outputs_given_inputs(input_data, output_data):
                     pass
             if errstr_liveliness_changed in strtemp or errstr_requested_deadline_missed in strtemp or errstr_sample_lost in strtemp:
                 if dumpallerrors:
-                    print("At time: " + pkt.sniff_timestamp + " : ", end='')
+                    print("At time: " + output_data[n][0] + " : ", end='')
                     print(strtemp, end='')
                 if errstr_liveliness_changed in strtemp:
                     error_type = [output_data[n][0], 1, 0, 0]
@@ -53,6 +55,32 @@ def split_data(split_percentage, data):
     return split1, split2
 
 
+def get_sd_mean(data):
+    sd = np.std(data, axis=0)
+    mean = np.mean(data, axis=0)
+    return sd, mean
+
+
+def standard_data(data, sd, mean):
+    std_data = data
+
+    for p in range(0, data.len()):
+        for d in range(0, data[0].len()):
+            std_data[p][d] = (data[p][d] - mean[d]) / sd[d]
+
+    return std_data
+
+
+def inverse_standard(data, sd, mean):
+    real_data = data
+
+    for p in range(0, data.len()):
+        for d in range(0, data[0].len()):
+            real_data[p][d] = (data[p][d] * sd[d]) + mean[d]
+
+    return real_data
+
+
 config = input_data.read_json("config.json")
 inputs, outputs = input_data.get_inputs_and_outputs(config["training_files"])
 
@@ -60,17 +88,35 @@ expected_outputs = outputs_given_inputs(inputs, outputs)
 training_in, testing_in = split_data(config["split"], inputs)
 training_out, testing_out = split_data(config["split"], expected_outputs)
 
-net = RNN.RNN(config, training_in, training_out)
+input_sd, input_mean = get_sd_mean(training_in)
+output_sd, output_mean = get_sd_mean(training_out)
+training_in_std = standard_data(training_in, input_sd, input_mean)
+training_out_std = standard_data(training_out, output_sd, output_mean)
+testing_in_std = standard_data(testing_in, input_sd, input_mean)
+testing_out_std = standard_data(testing_out, output_sd, output_mean)
+
+
+net = RNN.RNN(config, training_in_std, training_out_std)
 
 i = 0
 tf.random.set_seed(5)
-previous_error = tf.random.uniform(shape=[], minval=0, maxval=2)  # random error type
-previous_timestamp = tf.random.uniform(shape=[], minval=inputs[0][0], maxval=inputs[len(inputs)-1][0])  # random timestamp
+random_error = tf.random.uniform(shape=[], minval=0, maxval=2)  # random error type
+random_timestamp = tf.random.uniform(shape=[], minval=inputs[0][0], maxval=inputs[len(inputs)-1][0])  # random timestamp
+if random_error == 0:
+    previous_output = [random_timestamp, 1, 0, 0]
+elif random_error == 1:
+    random_output = [random_timestamp, 0, 1, 0]
+else:
+    random_output = [random_timestamp, 0, 0, 1]
+
 for pkt in inputs:
-    pkt.append(previous_error)
-    pkt.append(previous_timestamp)
+    pkt.extend(previous_output)
     actual_output = net.produce_output(pkt)
-    previous_error = actual_output[0]
-    previous_timestamp = actual_output[1]
     net.update(pkt, expected_outputs[i], actual_output)
+    previous_output = actual_output
     i = i + 1
+    # TODO print the output and related reward (accuracy)
+
+# TODO second loop for testing data
+
+# TODO save the weights and the sd/means
